f74f88afb03285b71185b6889d91fd4810aa2183
evince-dev-254 <evince.dev.254@gmail.com>
2026-01-14 17:19:15 +0300

    fix: add Tawk.to chat widget with correct credentials

--- app/layout.tsx
+++ app/layout.tsx
@@ -128,7 +128,7 @@ export default function RootLayout({
               (function(){
                 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
                 s1.async=true;
-                s1.src='https://embed.tawk.to/YOUR_TAWK_PROPERTY_ID/YOUR_WIDGET_ID';
+                s1.src='https://embed.tawk.to/6954cc957665ef197dae18b7/1jdpjuar6';
                 s1.charset='UTF-8';
                 s1.setAttribute('crossorigin','*');
                 s0.parentNode.insertBefore(s1,s0);
